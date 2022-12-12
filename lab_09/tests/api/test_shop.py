022d2db76b9c3ec092c9d78fa65074648761bc76
beta442 <grandoblivioniv@gmail.com>
2022-12-12 03:50:15 +0300

    `lab_09`: refactor

--- lab_09/tests/api/test_shop.py
+++ lab_09/tests/api/test_shop.py
@@ -1,17 +1,10 @@
 import pydantic
-import requests
 import unittest
 
 from sys import stderr
 
-from shop.api.common import ShopApiRouter, STATUS_CODE_OK
 from shop.api.models.product import ModelProduct as product_model
-
-
-# client = RequestsClient()
-# client = RequestsClient()
-# response = client.get('http://testserver/users/')
-# assert response.status_code == 200
+from shop.api.shop_api import ShopApi
 
 
 class TestShop(unittest.TestCase):
@@ -19,44 +12,23 @@ class TestShop(unittest.TestCase):
 	Shop's API test case
 	"""
 
-	products: list[product_model]
-
-	@staticmethod
-	def _make_request(request, url, params=None) -> requests.Response:
-		try:
-			return request(url, params=params)
-		except Exception as e:
-			print('Request exception:\n\t', e, file=stderr)
-
-	@staticmethod
-	def _delete_product(product_id) -> requests.Response:
-		response = TestShop._make_request(requests.get,
-		                                  ShopApiRouter.DELETE_PRODUCT_URL,
-		                                  {product_model.get_id_key(): product_id})
-		if response.status_code != STATUS_CODE_OK:
-			raise Exception(f'Failed to delete product with id "{product_id}"')
-		return response
-
-	@staticmethod
-	def _get_products() -> list[product_model]:
-		return TestShop._make_request(requests.get, ShopApiRouter.GET_ALL_PRODUCTS_URL).json()
-
 	def __init__(self, *args, **kwargs):
 		super(TestShop, self).__init__(*args, **kwargs)
+
+		self.__products: list[product_model] = list()
 
 		print('Starting shop''s Rest API tests')
 
 		self.addCleanup(self._delete_existing_products)
 		self.addCleanup(lambda: print('Deleting products after unit test is complete'))
 
-	@classmethod
-	def _delete_existing_products(cls) -> None:
-		products = TestShop._get_products() if len(cls.products) == 0 else cls.products
+	def _delete_existing_products(self) -> None:
+		products = ShopApi.get_products() if len(self.__products) == 0 else self.__products
 		for i, product in enumerate(products):
 			try:
-				parsed_product = cls.product_model.parse_obj(product)
+				parsed_product = product_model.parse_obj(product)
 				print(f'Deleteing product with id "{parsed_product.id}"')
-				TestShop._delete_product(parsed_product.id)
+				ShopApi.delete_product(parsed_product.id)
 			except pydantic.ValidationError as e:
 				print(f'Exception was thrown while deleting {i} product.\n' + e.json(), file=stderr)
 			except Exception as e:
@@ -64,7 +36,7 @@ class TestShop(unittest.TestCase):
 
 	@classmethod
 	def test_get_all_products(cls):
-		cls.assertEqual(1, 1)
+		cls.assertTrue(True)
 
 
 if __name__ == '__main__':
